65ee1b7533c53bd9e239fbde51e0d0aac28ba085
vehby <kurtcebevehbi@gmail.com>
2020-02-28 17:06:54 +0300

    Added style .scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/meterial';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
